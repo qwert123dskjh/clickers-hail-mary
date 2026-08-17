a23c24358680173259d6614e96d32804196619ac
qwerty123 <marcchauvet47@gmail.com>
2026-08-17 17:38:13 -0600

    Fix syntax error in setInterval callback

--- amirali.js
+++ amirali.js
@@ -227,7 +227,7 @@ document.addEventListener("DOMContentLoaded", () => {
         cookies += cursors;
                 saveGame();
                 updateDisplay();
-            }
+            })
         );
     }
 
